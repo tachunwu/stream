ec4da0a477e53b1c740b04a1d515e406df1cd271
tachunwu <tachunwu@gapp.nthu.edu.tw>
2023-02-28 14:05:23 +0800

    test in proc

--- js_local_server/js_local_server.go
+++ js_local_server/js_local_server.go
@@ -1,11 +1,8 @@
 package main
 
 import (
-	"crypto/tls"
 	"fmt"
-	"log"
-	"runtime"
-	"strings"
+	"sync"
 	"time"
 
 	natsserver "github.com/nats-io/nats-server/v2/server"
@@ -14,6 +11,15 @@ import (
 	natsclient "github.com/nats-io/nats.go"
 	"github.com/sirupsen/logrus"
 )
+
+var InProcessPubNum = 64
+var PublisherMsgs = 10000
+var wg = sync.WaitGroup{}
+var payload = "Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!"
+
+// Test result:
+// NATS Throughtput in process: 1.1509639323634913e+06
+// JetStream Throughtput in process: 1.0280622211398152e+06
 
 func main() {
 	s, err := natsserver.NewServer(&natsserver.Options{
@@ -25,7 +31,7 @@ func main() {
 		MaxPayload:      16 * 1024 * 1024,
 		NoSigs:          true,
 		NoLog:           false,
-		Trace:           true,
+		// Trace:           true,
 	})
 	if err != nil {
 		panic(err)
@@ -36,46 +42,64 @@ func main() {
 	if !s.ReadyForConnections(time.Second * 10) {
 		logrus.Fatalln("NATS did not start in time")
 	}
+
+	// In-Process
 	nc, err := natsclient.Connect("", natsclient.InProcessServer(s))
-	if err != nil {
+	if err != nil || nc == nil {
 		logrus.Fatalln("Failed to create NATS client")
 	}
-	if nc == nil {
-		var err error
-		opts := []natsclient.Option{}
-		opts = append(opts, natsclient.Secure(&tls.Config{
-			InsecureSkipVerify: true,
-		}))
 
-		nc, err = natsclient.Connect(strings.Join([]string{}, ","), opts...)
-		if err != nil {
-			logrus.WithError(err).Panic("Unable to connect to NATS")
-		}
+	// Test basic NATS
+	// Simple Async Subscriber
+	nc.Subscribe("foo", func(m *nats.Msg) {
+		_ = m.Data
+	})
+
+	wg.Add(InProcessPubNum)
+	start := time.Now()
+	for i := 0; i < InProcessPubNum; i++ {
+		go InProcessNATSPublisher(nc)
 	}
+	wg.Wait()
+	fmt.Println("NATS Throughtput in process:", float64(InProcessPubNum*PublisherMsgs)/time.Since(start).Seconds())
 
+	// Test HetStream
 	js, err := nc.JetStream()
 	if err != nil {
 		logrus.WithError(err).Panic("Unable to get JetStream context")
 	}
 
-	js.AddStream(&natsclient.StreamConfig{
-		Name:      "InProcess",
-		Retention: natsclient.InterestPolicy,
-		Storage:   natsclient.FileStorage,
+	// Create a Stream
+	js.AddStream(&nats.StreamConfig{
+		Name:     "ORDERS",
+		Subjects: []string{"ORDERS.*"},
 	})
-	info := <-js.Streams()
-	log.Println(
-		"Stream Name:", info.Config.Name,
-		"Stream Subject:", info.Config.Subjects,
-	)
-	// Test pub/sub
-	js.Publish("ORDERS.scratch", []byte("hello"))
+
+	// Simple Async Ephemeral Consumer
 	js.Subscribe("ORDERS.*", func(m *nats.Msg) {
-		fmt.Printf("Received a JetStream message: %s\n", string(m.Data))
-	}, natsclient.OrderedConsumer())
+		_ = m.Data
+	})
 
-	for {
-		runtime.Gosched()
+	wg.Add(InProcessPubNum)
+	start = time.Now()
+	for i := 0; i < InProcessPubNum; i++ {
+		go InProcessJetStreamPublisher(js)
 	}
+	wg.Wait()
+	fmt.Println("JetStream Throughtput in process:", float64(InProcessPubNum*PublisherMsgs)/time.Since(start).Seconds())
+}
 
+func InProcessNATSPublisher(nc *natsclient.Conn) {
+	defer wg.Done()
+	for i := 0; i < PublisherMsgs; i++ {
+		nc.Publish("foo", []byte(payload))
+	}
+}
+
+func InProcessJetStreamPublisher(js natsclient.JetStreamContext) {
+	defer wg.Done()
+	for i := 0; i < PublisherMsgs; i++ {
+		// Simple Stream Publisher
+		js.Publish("ORDERS.scratch", []byte(payload))
+	}
 }
